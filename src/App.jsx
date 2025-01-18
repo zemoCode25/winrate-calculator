a589c1e9f2694d699d43072f74833c61f6dcb290
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-01-18 16:33:15 +0800

    Initial commit with tailwind.css installation

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { WrCalculator } from "./components/WrCalculator";
+
+export function App() {
+  return (
+    <div className="w-dvw h-dvh flex items-center justify-center font-Inter">
+      <div className="flex flex-col items-center border border-gray-200 shadow-custom p-6 rounded-lg">
+        <h1 className="text-4xl font-bold text-violet-950">
+          Winrate Calculator
+        </h1>
+        <p className="mb-5 text-gray-600 text-sm">
+          Get your current and desired winrate
+        </p>
+        <WrCalculator />
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      robotoSlab: ["Roboto Slab", "sans-serif"],
+      Inter: ["Inter", "sans-serif"],
+    },
+    boxShadow: {
+      custom:
+        "0 5px 6px -1px rgba(128, 0, 128, 0.1), 0 2px 4px -1px rgba(128, 0, 128, 0.06)",
+    },
+  },
+  plugins: [],
+};
