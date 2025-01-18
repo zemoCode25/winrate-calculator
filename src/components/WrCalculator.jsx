c67960eddbd1d23350254edc84e2520c8436f95a
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-01-18 16:34:37 +0800

    Created main Component and winrate calculator component

---
+++ src/components/WrCalculator.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Result } from "./Result";
+import { Input } from "./Input";
+
+export function WrCalculator() {
+  const [calculator, setCalculator] = useState("current");
+  const [currentContent, setCurrentContent] = useState({
+    totalGames: 0,
+    winPercent: 0,
+  });
+  const [consecutiveWins, setConsecutiveWins] = useState(0);
+
+  function updateCurrentContent(inputTotalGames, computedWinPercent) {
+    setCurrentContent({
+      totalGames: inputTotalGames,
+      winPercent: computedWinPercent,
+    });
+  }
+
+  function updateDesiredContent(computedConsecutiveWins) {
+    setConsecutiveWins(computedConsecutiveWins);
+  }
+
+  function handleChangeCalcu(currentCalculator) {
+    setCalculator(currentCalculator);
+  }
+
+  return (
+    <main>
+      <div className="flex justify-between gap-7">
+        <Result
+          page={calculator}
+          content={{ ...currentContent, consecutiveWins }}
+        />
+        <Input
+          page={calculator}
+          handleChangeCalcu={handleChangeCalcu}
+          updateCurrentContent={updateCurrentContent}
+          updateDesiredContent={updateDesiredContent}
+        />
+      </div>
+    </main>
+  );
+}
