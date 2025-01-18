8418732c8164fa044a81bf405f098557a94b2e69
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-01-18 17:18:43 +0800

    Added footer credit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { WrCalculator } from "./components/WrCalculator";
 
 export function App() {
   return (
-    <div className="w-dvw h-dvh flex items-center justify-center font-Inter">
+    <div className="w-dvw h-dvh flex flex-col items-center justify-center font-Inter">
       <div className="flex flex-col items-center border border-gray-200 shadow-custom p-6 rounded-lg">
         <h1 className="text-4xl font-bold text-violet-950 text-center">
           Winrate Calculator
@@ -12,6 +12,18 @@ export function App() {
         </p>
         <WrCalculator />
       </div>
+      <footer className="mt-2">
+        <small>
+          Developed by{" "}
+          <a
+            className="font-semibold underline"
+            target="_blank"
+            href="https://github.com/zemoCode25"
+          >
+            Zemo
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
